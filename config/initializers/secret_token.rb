5edb3ab84320913bd73774c4064a004222630b11
Conrad Irwin <conrad.irwin@gmail.com>
2013-11-25 21:29:06 -0800

    Added new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CoffeeExperiments::Application.config.secret_key_base = 'edfd616df394701dacf7da27a4e809b4f297e1ddee77cdf4e9a362e1e7375cfdcd292666dedb22833b7f2a98548d322a83bd79327656111fc2f4c457461395c5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CoffeeExperiments::Application.config.session_store :cookie_store, key: '_coffee-experiments_session'
