b787544a649a35c5d20c8125ba3cf428825f15dc
Molly McNeil <molly.mcneil@gmail.com>
2014-12-02 18:05:11 -0800

    huge revamp of text, aeropress added

--- app/assets/javascripts/methods.js
+++ app/assets/javascripts/methods.js
@@ -1,55 +1,57 @@
-var water=1000;
-var ratio=14;
+var ratio;
+var water;
+function updateCalculator () {
+	console.log(water,ratio);
+	var servings_contents = $("#press-servings-dropdown [data-g=" + water + "]").html();
+	var size_contents = $("#press-size-dropdown [data-g=" + water + "]").html();
+	var ratio_contents = $("#press-ratio-dropdown [data-r=" + ratio + "]").html();
+
+	$("#press-size").html(size_contents);
+    $("#press-ratio").html(ratio_contents);
+	$("#press-beans").html(Math.round(water / ratio) + "g");
+	$("#press-water").html(water + "g");
+	$("#press-servings").html(servings_contents);	
+
+    if ($("div.calcapparatusimage > div").length > 1) {
+
+    	$("div.calcapparatusimage > div").hide();
+    	$("div.calcapparatusimage > div[data-g=" + water + "]").show();
+	}
+
+    $("div.calcwaterimage > div").hide();
+    $("div.calcwaterimage > div[data-g=" + water + "]").show();
+	$("div.calcbeanimage > div").hide();
+    $("div.calcbeanimage > div[data-r=" + ratio + "]").show();
+    $("div.calcmugimage > div").hide();
+    $("div.calcmugimage > div[data-g=" + water + "]").show();
+}
 
 $(function (){
+
+	water = $("#press-size").data("g");
+	ratio = $("#press-ratio").data("r");
+
+	updateCalculator();
+
 	$("#press-size-dropdown li").click(function(){
 		water = $(this).data("g");
 
-		var contents = $(this).html();
-		var servings_contents = $("#press-servings-dropdown [data-g=" + water + "]").html();
-
-		$("#press-size").html(contents);
-		$("#press-water").html(water + "g");
-		$("#press-beans").html(Math.round(water / ratio) + "g");
-		$("#press-servings").html(servings_contents);
-		$("div.calcmugimage > div").hide();
-        $("div.calcmugimage > div[data-g=" + water + "]").show();
-        $("div.calcwaterimage > div").hide();
-        $("div.calcwaterimage > div[data-g=" + water + "]").show();
-        $("div.calcapparatusimage > div").hide();
-        $("div.calcapparatusimage > div[data-g=" + water + "]").show();
+		updateCalculator();
 
 	})
 
 	$("#press-ratio-dropdown li").click(function(){
 		ratio = $(this).data("r");
 
-		var contents = $(this).html();
-
-		$("#press-ratio").html(contents);
-		$("#press-water").html(water + "g");
-		$("#press-beans").html(Math.round(water / ratio) + "g");
-		$("div.calcbeanimage > div").hide();
-        $("div.calcbeanimage > div[data-r=" + ratio + "]").show();
+		updateCalculator();
 
 	})
 
 	$("#press-servings-dropdown li").click(function(){
 		water = $(this).data("g");
 
-		var contents = $(this).html();
-		var size_contents = $("#press-size-dropdown [data-g=" + water + "]").html();
-                                                    
-		$("#press-servings").html(contents);
-		$("#press-water").html(water + "g");
-		$("#press-beans").html(Math.round(water / ratio) + "g");
-		$("#press-size").html(size_contents);
-		$("div.calcmugimage > div").hide();
-        $("div.calcmugimage > div[data-g=" + water + "]").show();
-        $("div.calcwaterimage > div").hide();
-        $("div.calcwaterimage > div[data-g=" + water + "]").show();
-        $("div.calcapparatusimage > div").hide();
-        $("div.calcapparatusimage > div[data-g=" + water + "]").show();
+		updateCalculator();
+
 	})
 
    
